434aa9d823e657d6f54decccb7ea4fcca8cb74fe
Joel Oliveira <joel@thoughtbot.com>
2013-03-17 19:28:43 -0400

    [JRO] send Resque::Mailer to mailers on initialization

--- lib/generators/thredded_resque/install/install_generator.rb
+++ lib/generators/thredded_resque/install/install_generator.rb
@@ -4,7 +4,6 @@ module ThreddedResque
   module Generators
     class InstallGenerator < Rails::Generators::Base
       include Rails::Generators::Migration
-      # source_root File.expand_path('../templates', __FILE__)
 
       def add_worker_to_procfile
         append_file 'Procfile', "worker: bundle exec rake environment resque:work QUEUE='*'"
--- lib/thredded_resque/engine.rb
+++ lib/thredded_resque/engine.rb
@@ -4,5 +4,11 @@ require 'rails'
 
 module ThreddedResque
   class Engine < Rails::Engine
+    initializer 'thredded_resque.extend_mailers' do |app|
+      app.config.to_prepare do
+        PostMailer.send(:include, Resque::Mailer)
+        TopicMailer.send(:include, Resque::Mailer)
+      end
+    end
   end
 end
